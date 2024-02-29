0d37f46a434715180746bfb9f3bd724f6f0d79b0
Gabriel dos Santos <lopesdeveloperweb@gmail.com>
2024-02-29 08:38:30 -0300

    resolução - exercício 34

---
+++ lista-exercicios-fit/resolucoes/34.js
@@ -0,0 +1,31 @@
+x = 50
+y = 3
+z = (x * y) + 5
+console.log(z)
+
+if (z <= 0) {
+
+    resposta = "A"
+
+} else {
+
+    if(z <= 100) {
+
+        resposta = "B"
+
+    } else {
+        
+        resposta = "C"
+
+    }
+}
+
+console.log(resposta)
+
+// 01- z = 11 resposta = "B"
+// 02- z = 455 resposta = "C"
+// 03- z = -2 resposta = "A"
+// 04- z = -5 resposta = "A"
+// 05- z = 155 resposta = "C"
+
+
